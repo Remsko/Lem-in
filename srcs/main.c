65eb2777bd9bbbfc234fc985b35810b9cda56210
Remsko <pinoit.remi@gmail.com>
2018-12-19 11:34:00 +0100

    feat: some preicse error handling

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 17:06:33 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/12/13 17:47:14 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/12/19 11:32:50 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		main(void)
 
 	ft_bzero(&env, sizeof(t_env));
 	if ((env.anthill = (t_anthill *)array_create(sizeof(char *))) == NULL)
-		error_malloc();
+		error_malloc("main line 30.");
 	if (parser_all(&env) == true)
 	{
 		anthill_print(env.anthill);
